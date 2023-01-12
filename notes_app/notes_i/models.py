799306265a81c4a06cbc8b1202db5e54762952d2
Ihor Bokii <iabokiy@gmail.com>
2023-01-12 21:08:08 +0200

    Add models, update index.html

--- notes_app/notes_i/models.py
+++ notes_app/notes_i/models.py
@@ -7,12 +7,18 @@ class Category(models.Model):
 	def __str__(self):
 		return self.tittle
 
+	class Meta:
+		verbose_name_plural = "Notes"
+
 
 class Notes(models.Model):
 	tittle = models.CharField(max_length=100)
 	text = models.TextField(null=True, blank=True)
 	reminder = models.DateTimeField(null=True)
 	category = models.ForeignKey(Category, on_delete=models.DO_NOTHING, null=True)
+
+	class Meta:
+		verbose_name_plural = "Categories"
 
 	def __str__(self):
 		return self.tittle
--- notes_app/notes_i/views.py
+++ notes_app/notes_i/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Notes, Category
 
 
 # Create your views here.
 def index(request):
-	return render(request, 'index.html')
+	notes = Notes.objects.all()
+	# 1. Count notes for all categories.
+	# notes_count = Category.objects.count()
+	# print(notes_count)
+	# 2.
+	context = {'notes': notes}
+	return render(request, 'index.html', context=context)
